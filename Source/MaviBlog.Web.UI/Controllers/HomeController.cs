ac0580c5a72e107eea71e29e36b1269b708527e9
Brian Mavity <bmavity@brianmavity.com>
2009-11-11 01:00:49 -0600

    Initial commit of blog project

---
+++ Source/MaviBlog.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MaviBlog.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/MaviBlog.Web.UI/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Spark.Web.Mvc;
+
+namespace MaviBlog.Web.UI
+{
+    public class MvcApplication : HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = "" });
+
+            ViewEngines.Engines.Add(new SparkViewFactory());
+        }
+
+        protected void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
