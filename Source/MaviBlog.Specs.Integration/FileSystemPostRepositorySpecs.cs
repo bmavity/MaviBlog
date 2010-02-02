2dccd3c74f6a90cf9af863c6ebb9627c2b8abf75
Brian Mavity <bmavity@brianmavity.com>
2010-02-02 08:35:47 -0600

    Implemented file system post repository for multiple posts

--- Source/MaviBlog.Specs.Integration/FileSystemPostRepositorySpecs.cs
+++ Source/MaviBlog.Specs.Integration/FileSystemPostRepositorySpecs.cs
@@ -6,11 +6,11 @@ namespace MaviBlog.Specs.Integration
 {
     public class FileSystemPostRepositorySpecs {}
 
-    [Subject(typeof(FileSystemPostRepository)), Tags("developer", "creating post")]
-    public class when_saving_the_first_post : empty_post_repository
+    [Subject("An empty FileSystemPostRepository"), Tags("developer", "creating post")]
+    public class when_saving_a_post : empty_post_repository
     {
         private static long postId;
-        private static Post postToSave = new Post
+        private static readonly Post postToSave = new Post
         {
             Author = "George RR Martin",
             Content = "<h1>I'm still not finished</h1>",
@@ -24,8 +24,50 @@ namespace MaviBlog.Specs.Integration
         It should_create_the_data_file = () =>
             dataFile.ShouldExist();
 
+        It should_set_the_post_id_to_one = () =>
+            postId.ShouldEqual(1);
+
         It should_contain_the_first_entry = () =>
             repository.GetPostById(postId).ShouldMatch(postToSave);
+    }
+
+    [Subject("FileSystemPostRepository that contains posts"), Tags("developer", "creating post")]
+    public class when_saving_an_additional_post : empty_post_repository
+    {
+        private static long firstPostId;
+        private static long additionalPostId;
+        private static readonly Post firstPost = new Post
+        {
+            Author = "George RR Martin",
+            Content = "<h1>I'm still not finished</h1>",
+            PublishDate = "10/15/2090",
+            Title = "A Dance With Dragons",
+        };
+
+        private static readonly Post additionalPost = new Post
+        {
+            Author = "Brian Mavity",
+            Content = "<h1>I'm still not blogging</h1>",
+            PublishDate = "7/5/2010",
+            Title = "*sadface*",
+        };
+
+        Establish context = () =>
+        {
+            firstPostId = repository.Save(firstPost);
+        };
+
+        Because of = () =>
+            additionalPostId = repository.Save(additionalPost);
+
+        It should_increment_the_id_for_the_additional_post = () =>
+            additionalPostId.ShouldEqual(2);
+
+        It should_contain_the_additional_entry = () =>
+            repository.GetPostById(additionalPostId).ShouldMatch(additionalPost);
+
+        It should_contain_the_first_entry = () =>
+            repository.GetPostById(firstPostId).ShouldMatch(firstPost);
     }
 
     public class empty_post_repository
--- Source/MaviBlog/FileSystemPostRepository.cs
+++ Source/MaviBlog/FileSystemPostRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web.Script.Serialization;
 
 namespace MaviBlog
@@ -21,31 +22,47 @@ namespace MaviBlog
 
         public PostViewModel GetPostById(long id)
         {
-            using (var reader = new StreamReader(_filePath))
-            {
-                var post = new JavaScriptSerializer().Deserialize<PostViewModel>(reader.ReadLine());
-                reader.Close();
-                return post;
-            }
+            return GetAllPosts()
+                .ElementAt((int) id - 1);
         }
 
         public long Save(Post postToCreate)
         {
+            CreateFileIfItDoesNotExist();
+            var nextId = GetAllPosts().Count() + 1;
             using (var writer = GetFileWriter())
             {
                 writer.WriteLine(new JavaScriptSerializer().Serialize(postToCreate));
                 writer.Close();
             }
-            return 1;
+            return nextId;
         }
 
         private StreamWriter GetFileWriter()
         {
+            return new StreamWriter(_filePath, true);
+        }
+
+        private IEnumerable<PostViewModel> GetAllPosts()
+        {
+            var posts = new List<PostViewModel>();
+            using (var reader = new StreamReader(_filePath))
+            {
+                while(!reader.EndOfStream)
+                {
+                    posts.Add(new JavaScriptSerializer().Deserialize<PostViewModel>(reader.ReadLine()));
+                }
+                reader.Close();
+            }
+            return posts;
+        }
+
+        private void CreateFileIfItDoesNotExist()
+        {
             if (!File.Exists(_filePath))
             {
-                return new StreamWriter(File.Create(_filePath));
+                using (File.Create(_filePath)) ;
             }
-            return new StreamWriter(_filePath, true);
         }
     }
 }
