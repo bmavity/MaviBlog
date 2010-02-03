f863de442c915b851f01f3783d6875206781c048
Brian Mavity <bmavity@brianmavity.com>
2010-02-03 08:51:30 -0600

    Implemented comments for individual posts

--- Source/MaviBlog.Web/Controllers/Home/HomeController.cs
+++ Source/MaviBlog.Web/Controllers/Home/HomeController.cs
@@ -1,4 +1,7 @@
-﻿namespace MaviBlog.Web.Controllers.Home
+﻿using MaviBlog.Web.Controllers.Post;
+using StructureMap;
+
+namespace MaviBlog.Web.Controllers.Home
 {
     public class HomeController
     {
@@ -11,6 +14,23 @@
 
         public HomeViewModel Index()
         {
+            //var posts = ObjectFactory.GetInstance<PostController>();
+            //posts.Post(new PostCreateInputModel
+            //{
+            //    Author = "George RR Martin",
+            //    Content = "<h1>I'm still not finished</h1>",
+            //    PublishDate = "10/15/2090",
+            //    Title = "A Dance With Dragons",
+            //});
+
+            //posts.Post(new PostCreateInputModel
+            //{
+            //    Author = "Brian Mavity",
+            //    Content = "<h1>I'm still not blogging</h1>",
+            //    PublishDate = "7/5/2010",
+            //    Title = "*sadface*",
+            //});
+
             return new HomeViewModel
             {
                 Posts = _repository.GetLatestPosts(),
