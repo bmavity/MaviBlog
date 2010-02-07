625ada0e86246b0de0535e3c6201d16d1b083093
Brian Mavity <bmavity@brianmavity.com>
2010-02-07 07:49:49 -0600

    modified test.html to use a text area for content and extracted a base class for title url encoder specs

--- Source/MaviBlog.Specs/Core/TitleUrlEncoderSpecs.cs
+++ Source/MaviBlog.Specs/Core/TitleUrlEncoderSpecs.cs
@@ -1,20 +1,13 @@
 using Machine.Specifications;
 
+// ReSharper disable InconsistentNaming
 namespace MaviBlog.Specs.Core
 {
     public class TitleUrlEncoderSpecs {}
 
     [Subject(typeof(TitleUrlEncoder))]
-    public class when_encoding_a_title_with_capital_letters
+    public class when_encoding_a_title_with_capital_letters : url_encoder
     {
-        private static TitleUrlEncoder encoder;
-        private static string encodedTitle;
-
-        Establish context = () =>
-        {
-            encoder = new TitleUrlEncoder();
-        };
-
         Because of = () =>
             encodedTitle = encoder.EncodeTitle("ThisIsThePostTitleWithCapitalLetters");
 
@@ -23,20 +16,24 @@ namespace MaviBlog.Specs.Core
     }
 
     [Subject(typeof(TitleUrlEncoder))]
-    public class when_encoding_a_title_with_spaces
+    public class when_encoding_a_title_with_spaces : url_encoder
     {
-        private static TitleUrlEncoder encoder;
-        private static string encodedTitle;
-
-        Establish context = () =>
-        {
-            encoder = new TitleUrlEncoder();
-        };
-
         Because of = () =>
             encodedTitle = encoder.EncodeTitle("this is a title with spaces");
 
         It should_replace_all_spaces_with_dashes = () =>
             encodedTitle.ShouldEqual("this-is-a-title-with-spaces");
     }
+
+    public class url_encoder
+    {
+        protected static TitleUrlEncoder encoder;
+        protected static string encodedTitle;
+
+        Establish context = () =>
+        {
+            encoder = new TitleUrlEncoder();
+        };
+    }
 }
+// ReSharper restore InconsistentNaming
